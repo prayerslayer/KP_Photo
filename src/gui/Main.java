2e4d8cfc37133a047e23e927535b5a17e10acd40
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-02-15 23:32:25 +0100

    first commit incl boofcv lib

---
+++ src/gui/Main.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JWindow;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PhotoWindow photo = new PhotoWindow( );
+		photo.setBounds(0, 0, 200, 200 );
+		photo.setTitle( "Blublu");
+		photo.setVisible( true );
+	}
+
+}
