f055ee93ef097dbc0e41443c9aa04be556d92ee2
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-03-06 18:10:42 +0100

    save panorama

--- src/gui/subviews/blend/BlendPanel.java
+++ src/gui/subviews/blend/BlendPanel.java
@@ -1,13 +1,20 @@
 package gui.subviews.blend;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import gui.subviews.SubView;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import photo.OrientationFailedException;
 import photo.StitcherFacade;
@@ -16,22 +23,48 @@ public class BlendPanel extends SubView {
 	
 	private JLabel lbImage;
 	private JButton btSave;
+	private BufferedImage pano;
 	
 	public BlendPanel() {
+		setLayout(new BorderLayout(0, 0));
 		
 		lbImage = new JLabel();
 		lbImage.setHorizontalAlignment( JLabel.CENTER );
-		add(lbImage, BorderLayout.CENTER );
+		lbImage.setVerticalAlignment( JLabel.CENTER );
+		add(lbImage );
 		
+		final JPanel parent = this;
 		btSave = new JButton();
-		add( btSave, BorderLayout.SOUTH );
+		btSave.setIcon( new ImageIcon( getClass().getResource( "/gui/images/save.png" ) ) );
+		btSave.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent evt ) {
+				if ( pano == null ) {
+					//TODO show message pane
+					return;
+				}
+				
+				JFileChooser saveDlg = new JFileChooser();
+				saveDlg.setMultiSelectionEnabled( false );
+				int action = saveDlg.showSaveDialog( parent );
+				if ( action == JFileChooser.APPROVE_OPTION ) {
+					File file = saveDlg.getSelectedFile();
+					try {
+						ImageIO.write( pano, "png", file );
+					} catch (IOException e) {
+						e.printStackTrace();
+						//TODO show message pane
+					}
+				}
+			}
+		});
+		add( btSave );
 	}
 
 	@Override
 	public void init() {
 		controller = new BlendController( this, StitcherFacade.getInstance() );
 		try {
-			BufferedImage pano = ((BlendController)controller).makePanorama( getWidth(), getHeight() );
+			pano = ((BlendController)controller).makePanorama( getWidth(), getHeight() );
 			lbImage.setIcon( new ImageIcon( pano ) );
 		} catch (OrientationFailedException e) {
 			// TODO Auto-generated catch block
